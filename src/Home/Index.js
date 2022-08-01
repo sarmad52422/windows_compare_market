a2f0324668ef22eddb8b4d702001d9480a12f347
sarmad <sarmad.52422@gmail.com>
2022-08-01 22:45:15 +0500

    check commit

--- src/Home/Index.js
+++ src/Home/Index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Home(){
     return (
-        <h1>Changes Deployed to Heroku automatically</h1>
+        <h1>Yayeeeeeeeee i am on home page</h1>
     )
 }
