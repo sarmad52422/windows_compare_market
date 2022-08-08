955e705bc63b5326556379c07867ae3774a1fa4a
sarmad <sarmad.52422@gmail.com>
2022-08-08 18:17:16 +0500

    simpel nodemon

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api',{
-            target: 'http://localhost:'+process.env.PORT,
+            target: 'http://localhost:'+(process.env.PORT || 3100),
             changeOrigin: true,
         })
     );
