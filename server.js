aab64721c115343cf573bfac51ad2b406c34117d
sarmad <sarmad.52422@gmail.com>
2022-08-08 18:43:57 +0500

    server deployed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3100
 app.listen(port,()=>{
@@ -9,3 +10,7 @@ app.listen(port,()=>{
 app.get('/api',(req,res)=>{
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 })
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
+app.use(express.static(path.join(__dirname,'build')))
