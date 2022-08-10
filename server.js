336d3b9cb24b8906d61b8727eefb8ca0ce39d1d5
sarmad <sarmad.52422@gmail.com>
2022-08-10 18:09:20 +0500

    server deployed

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const userRouter = require('./src/server/routers/user_router')
 const port = process.env.PORT || 3100
 
 
+app.use('/api/users', userRouter)
 
 
-app.use('/api/users',userRouter)
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'build','index.html'))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.get('*',(req,res)=>{
-    res.send({express:"404 Not Found"})
+
+
+app.get('*', (req, res) => {
+    res.send({express: "404 Not Found"})
 })
-
-
-app.use(express.static(path.join(__dirname,'build')))
 
 app.listen(port, () => {
     console.log("Server is running on " + port)
